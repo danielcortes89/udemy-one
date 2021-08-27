a5cbf8e99c7c2c9b6459e8a70d2576a19ffed44a
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-08-26 19:53:26 -0500

    feat: Added user page and call

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import Navbar from './components/layout/Navbar'
+
 import Users from './components/users/Users'
+import User from './components/users/User'
+
 import Search from './components/users/Search'
 import Alert from './components/layout/Alert'
 import About from './components/pages/About'
+
 
 import axios from 'axios'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
@@ -14,6 +18,7 @@ import './App.css';
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null
 	}
@@ -25,6 +30,18 @@ class App extends Component {
 		const res = await axios.get(`https://api.github.com/search/users?q=${searchTerm}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&clinet_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
 
 		this.setState({ users: res.data.items, loading: false })
+	}
+
+	// Get a single githug user
+	getUser = async (username) => {
+		this.setState({
+			loading: true
+		})
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&clinet_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+		this.setState({ user: res.data, loading: false })
 	}
 
 	clearUsers = () => {
@@ -46,7 +63,7 @@ class App extends Component {
 	}
 
 	render(){
-		const { users, loading } = this.state
+		const { users, loading, user } = this.state
 
 		return (
 			<Router>
@@ -71,6 +88,12 @@ class App extends Component {
 							</Fragment>
 						)}/>
 						<Route exact path="/about" component={About}/>
+						<Route 
+							exact 
+							path='/user/:login' 
+							render={props => (
+							<User {...props} getUser={this.getUser} user={user} loading={loading}/>
+							)}/>
 					</Switch>
 					
 				</div>
